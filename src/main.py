231287fee6d5d99f6494ddf2c67ea0201bc8adcf
Dylan <d.barva5402@gmail.com>
2020-07-25 19:36:17 -0700

    work on config file

--- src/main.py
+++ src/main.py
@@ -3,22 +3,33 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import tbapy
 import tbapy.models
+import jsbeautifier
 
 import keys
 
-# dict = json.load(open("config.json"))
+# dict = json.load(open("config_ex.json"))
 # print(dict.get("data"))
 
 
-def gen_config_file_with_tba(input_sheet : str, output_sheet : str, event : str, tba_key : str):
-    gen_config_file(input_sheet, output_sheet, get_team_list(event, tba_key))
+def gen_config_file_with_tba(input_sheet : str, output_sheet : str, event : str, tba_key : str, google_json : str):
+    gen_config_file(input_sheet, output_sheet, get_team_list(event, tba_key), google_json)
 
 
-def gen_config_file(input_sheet : str, output_sheet : str, team_list : list):
+def gen_config_file(input_sheet : str, output_sheet : str, team_list : list, google_json : str):
+    config_dict = {}
     scope = ['https://spreadsheets.google.com/feeds',
              'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name("NerdyScoutBlitz-52160ecf0355.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(google_json, scope)
     client = gspread.authorize(creds)
+    input = client.open(input_sheet).sheet1
+    input_data = input.get_all_values()
+    config_dict.update({"input_headers" : input_data[0]})
+    config_dict.update({"teams" : team_list})
+
+    opts = jsbeautifier.default_options()
+    opts.indent_size = 2
+    with open('config.json', 'w') as fp:
+        jsbeautifier.beautify(json.dump(config_dict, fp), opts)
 
 
 def get_team_list(event : str, tba_key : str) -> list:
@@ -31,5 +42,6 @@ def get_team_list(event : str, tba_key : str) -> list:
         team_list.append(team_num)
     return sorted(team_list)
 
-print(get_team_list("2020cala", keys.key))
-
+# print(get_team_list("2020cala", keys.key))
+# NerdyScout2Input
+gen_config_file_with_tba("NerdyScout2Input", "NerdyScout2Output", "2020cala", keys.key, "NerdyScoutBlitz-52160ecf0355.json")
